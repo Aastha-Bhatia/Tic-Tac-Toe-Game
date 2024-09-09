e53ba9b651ac21032cba4eea6955ae5484eddbac
Aastha Bhatia <122430152+Aastha-Bhatia@users.noreply.github.com>
2024-09-09 09:34:42 +0530

    Update App.java

--- TicTacToe/TicTacToe/src/App.java
+++ TicTacToe/TicTacToe/src/App.java
@@ -1,3 +1,8 @@
+// TicTacToe Game
+// Developed by: Aastha Bhatia
+// Description: A simple Tic-Tac-Toe game implementation using Java Swing.
+// Feel free to modify and distribute, but please retain this attribution.
+
 public class App {
     public static void main(String[] args) throws Exception {
         TicTacToe ticTacToe=new TicTacToe();
